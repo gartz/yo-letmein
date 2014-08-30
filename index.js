a2cfebc4168c64be986d85824456fd2a48f0dc01
Gabriel Reitz Giannattasio <gartz@l6.com.br>
2014-08-30 00:01:16 -0700

    Read the security, and wait for http request to run gpio

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var Yo = require('yo-api');
+var security = require('./security.json');
+
+var app = express();
+
+app.get('/', function(req, res){
+    var user = req.query.username ? req.query.username.toUpperCase() : '';
+    if (!security[user]) {
+        console.log('Security fail for: %s', user);
+        return;
+    }
+    console.log('Opening the door for: %s', user);
+    try {
+        var gpio = require("pi-gpio");
+        gpio.open(12, "output", function(err) {     // Open pin 12 for output
+            gpio.write(12, 1, function() {          // Set pin 12 high (1)
+                setTimeout(gpio.close.bind(gpio, 16), 6e3);
+            });
+        });
+    } catch(e) {
+        console.warn('GPIO FAIL');
+    }
+    res.send('yo brow');
+});
+
+var server = app.listen(8880, function() {
+    console.log('Listening on port %d', server.address().port);
+});

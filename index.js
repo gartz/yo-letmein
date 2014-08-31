6a4bcc8f9a3ce30d69df8b9b3a2ab74a8d1dc73e
Gabriel Reitz Giannattasio <gartz@l6.com.br>
2014-08-30 19:27:24 -0700

    Added help options to command line

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+var packagejson = require('./package.json');
 var fs = require('fs');
 var net = require('net');
 var http = require('http');
 var https = require('https');
 
-var argv = require('yargs')
+var yargs = require('yargs')
     .default('listen', 8880)
+    .alias('l', 'listen')
     .default('http', './http.sock')
     .default('https', './https.sock')
     .default('key', 'sslcert/server.key')
+    .alias('k', 'key')
     .default('crt', 'sslcert/server.crt')
+    .alias('c', 'crt')
     .boolean('insecure')
+    .alias('i', 'insecure')
     .boolean('httpsOff')
-    .argv;
+    .alias('v', 'version')
+    .version(packagejson.version, 'version')
+    .showHelpOnFail(false, "Specify --help for available options");
+
+var argv = yargs.argv;
+if (argv.help) {
+    console.log(yargs.help());
+    return;
+}
+
 
 var listen = argv.listen;
 var httpAddress = argv.http;
